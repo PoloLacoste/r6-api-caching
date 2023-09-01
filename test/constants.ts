821edb6c012a2e6c124983a66216a7bda3969c2a
JM REMEUR <jm@remeur.fr>
2023-09-01 21:30:52 +0200

    bump deps added prettierrc migrate to pnpm

--- test/constants.ts
+++ test/constants.ts
@@ -1,6 +1,7 @@
 export const username = 'MakeMyHole.WAFL';
 export const platform = 'uplay';
 export const userId = 'd23837fa-f951-4781-9d22-29a23671f4de';
+export const seasonId = 24;
 export const seasonName = 'High Calibre';
 export const seasonCurrentMMR = 3054;
 export const seasonKills = 1140;
